34ce456bdd71d82c3119e124411c382d2d3ce8f8
joe <joe>
2003-08-14 23:19:34 +0000

    added some notion of security (won't display anything on the filesystem) and performance (spew rather than spawning a cat)

--- src/cgi/retrieveXML.pl
+++ src/cgi/retrieveXML.pl
@@ -22,9 +22,55 @@ my $name = $cgi->param('name');
 
 $name =~ s/\.xml$//;
 
-print $cgi->header("application/x-apollo");
+# strip off any possible directory references. It's not
+# polite to try to hack in.
+$name =~ s:.*[./]::;
 
-my $filename = "$PELEMENT_XML/$name.xml";
+&error($cgi,"<No scaffold specified>") unless $name;
 
-print `cat $filename` if -e $filename;
+if ($name eq 'pelementXML') {
+   # special case. The whole thing.
+   my $filename = "$PELEMENT_XML/pelementXML.tar.gz";
+   &error($cgi,$name) unless -e $filename;
+   print $cgi->header(-type=>"application/gzip-compressed",-content_disposition=>"attachment; filename=pelementXML.tar.gz");
+   spew($filename);
+} else {
+   my $filename = "$PELEMENT_XML/$name.xml";
+   &error($cgi,$name) unless -e $filename;
+   print $cgi->header(-type=>"application/x-apollo",-content_disposition=>"attachment; filename=$name.xml");
+   spew($filename);
+}
 
+sub spew
+{
+   my $filename = shift;
+   my $buffer;
+
+   # the size of our I/O hunk
+   my $bufferSize = 5000;
+
+   open(FIL,"$filename") or return;
+
+   while( read(FIL,$buffer,$bufferSize) ) {
+      print $buffer;
+   }
+
+   close(FIL);
+}
+
+sub error
+{
+   my $cgi = shift;
+   my $noFound = shift;
+   
+   print $cgi->header();
+   print $cgi->init_page();
+   print $cgi->banner();
+
+   print $cgi->h3("Cannot find the file for $name."),"\n";
+
+   print $cgi->footer();
+   print $cgi->close_page();
+
+   exit(0);
+}
