49b8ad796675dc7996566067f7ffb4e13b5dd8c2
joe <joe>
2003-09-24 00:40:42 +0000

    change mapping insertions in cases insert is outside the HSP

--- src/perl/alignSeq.pl
+++ src/perl/alignSeq.pl
@@ -30,6 +30,11 @@ my $session = new Session({-log_level=>$Session::Verbose});
 
 my $seq_name = $ARGV[0];
 
+# a configurable parameter of whether to extrapolate the insertion
+# position beyond the HSP. If false, the last position of the HSP
+# is reported.
+my $extrapolate = 0;
+
 # todo: change this in to a BlastInterface object
 my $sql = qq(select blast_hsp.id,name,score,percent,match,length,query_begin,
              query_end,subject_begin,subject_end,strand,query_align,subject_align
@@ -42,12 +47,15 @@ my $sql = qq(select blast_hsp.id,name,score,percent,match,length,query_begin,
 my @records = ();
 $session->db->select($sql,\@records);
 
-# todo: change this into a Sequence object
 my $seq = new Seq($session,{-seq_name=>$seq_name})->select;
 
 my $seq_length = length($seq->sequence);
 
-exit(1) if $seq_length < $length_threshold;
+if ($seq_length < $length_threshold ) {
+   $session->log($Session::Info,"Sequence length is below the threshold.");
+   $session->exit;
+   exit(0);
+}
 
 my $insert_pos = $seq->insertion_pos;
 
@@ -78,13 +86,16 @@ while (@records) {
       # we need to count into the alignments for the positions.
       # this can probably be made more compact, but this makes the
       # mapping steps explicit.
-      # if the insert is exactly at the end of the hps, this is
+      # if the insert is exactly at the end of the hsp, this is
       # easy
       my $s_insert = 0;
+      # cases 1 and 2 are when the insert is at the end. This is simplest
       if( $insert_pos == $query_begin) {
          $s_insert = $subject_begin;
       } elsif ($insert_pos == $query_end) {
          $s_insert = $subject_end;
+      # the next case in somewhere in the middle. We'll count matches to
+      # find the bestest mapping
       } elsif( ($insert_pos > $query_begin && $insert_pos < $query_end) ||
           ($insert_pos < $query_begin && $insert_pos > $query_end) ) {
          $s_insert = $subject_begin;
@@ -95,18 +106,29 @@ while (@records) {
             $s_insert++ if substr($s_align,$i-1,1) ne '-';
             last if $q_pos==$insert_pos;
          }
+      # the next 4 cases are beyond the end. Originally I extrapolated
+      # for the reported position. Now I don't think that is correct.
+      # (and others share that view.)
       } elsif ( $query_end > $query_begin && $insert_pos > $query_end) {
          # beyond the + end with a + hit
-         $s_insert = $subject_end + ($insert_pos - $query_end);
+         $s_insert = $subject_end;
+         $s_insert = $subject_end + ($insert_pos - $query_end)
+                                                      if ($extrapolate)
       } elsif ( $query_end > $query_begin && $insert_pos < $query_begin ) {
          # beyond the - end with a + hit
-         $s_insert = $subject_begin - ($query_begin - $insert_pos);
+         $s_insert = $subject_begin;
+         $s_insert = $subject_begin - ($query_begin - $insert_pos)
+                                                      if ($extrapolate)
       } elsif ( $query_end < $query_begin && $insert_pos < $query_end) {
          # beyond the - end with a - hit
-         $s_insert = $subject_end + ($query_end - $insert_pos);
+         $s_insert = $subject_end;
+         $s_insert = $subject_end + ($query_end - $insert_pos)
+                                                      if ($extrapolate)
       } elsif ( $query_end < $query_begin && $insert_pos > $query_begin) {
          # beyond the + end with a - hit
-         $s_insert = $subject_begin - ($insert_pos - $query_begin);
+         $s_insert = $subject_begin;
+         $s_insert = $subject_begin - ($insert_pos - $query_begin)
+                                                      if ($extrapolate)
       } else {
          die "internal inconsistency with insert position.";
       }
@@ -127,8 +149,12 @@ $session->db->do($sql) if $sql;
 
 my $numHits = $session->db->select_value("select count(seq_name) from seq_alignment where ".
                                          "seq_name='$seq_name'");
-$session->db->do("update seq_alignment set status='unique' where seq_name='$seq_name' ".
-                                             "and status is null") if ($numHits == 1);
+if( $numHits == 1 ) {
+   $session->log($Session::Verbose,"Declaring this alignment unique.");
+   $session->db->do(qq(update seq_alignment set status='unique' where
+                       seq_name='$seq_name' and status='multiple'));
+}
+
 $session->exit();
 
 exit(0);
