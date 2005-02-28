df18f7f3b4bf714540dd9813e09b17245273186f
joe <joe>
2005-02-28 18:47:20 +0000

    conversion to blast objects

--- src/perl/alignSeq.pl
+++ src/perl/alignSeq.pl
@@ -17,72 +17,111 @@ use Seq;
 use Seq_Alignment;
 use Seq_AlignmentSet;
 use Session;
-#use BlastHsps;
-#use BlastInterface;
+use Blast_Report;
+use Blast_ReportSet;
 use Files;
 use strict;
-no strict 'refs';
+
+use Getopt::Long;
 
 my $percent_threshold = 95;
 my $length_threshold = 25;
-
-my $session = new Session({-log_level=>$Session::Verbose});
-
-my $seq_name = $ARGV[0];
+my $test = 0;
+my $verbose = 0;
 
 # a configurable parameter of whether to extrapolate the insertion
 # position beyond the HSP. If false, the last position of the HSP
-# is reported.
-my $test = 0;
+# is reported. We probably should never change this
 my $extrapolate = 0;
 
-# todo: change this in to a BlastInterface object
-my $sql = qq(select blast_hsp.id,name,score,percent,match,length,query_begin,
-             query_end,subject_begin,subject_end,strand,query_align,subject_align
-             from blast_run, blast_hit,blast_hsp where
-             blast_run.seq_name=).$session->db->quote($seq_name).
-          qq( and blast_run.id=run_id and blast_hit.id=hit_id
-             and blast_run.db='release3_genomic'
-             order by score desc);
+# do we automatically delete old alignments? Normally we want
+# to let transferCurations do this
+my $deleteOld = 0;
 
-my @records = ();
-$session->db->select($sql,\@records);
+GetOptions( 'percent=i'    => \$percent_threshold,
+            'length=i'     => \$length_threshold,
+            'test!'        => \$test,
+            'verbose!'     => \$verbose,
+            'extrapolate!' => \$extrapolate,
+            'delete!'      => \$deleteOld,
+           );
 
+my $session = new Session;
+$session->log_level($Session::Verbose) if $verbose;
+
+my $seq_name = $ARGV[0];
+
+# some preliminaries to make sure we have a legit seq
 my $seq = new Seq($session,{-seq_name=>$seq_name})->select;
-
 my $seq_length = length($seq->sequence);
 
-if ($seq_length < $length_threshold ) {
-   $session->log($Session::Info,"Sequence length is below the threshold.");
-   $session->exit;
-   exit(0);
-}
-
+$session->die("Sequence length is below the threshold.")
+                       if ($seq_length < $length_threshold );
+   
 my $insert_pos = $seq->insertion_pos;
+$session->die("Sequence does not have an insertion position.")
+                                   unless (defined($insert_pos));
 
-# out with the old
-my $sSet = new Seq_AlignmentSet($session,{-seq_name=>$seq_name})->select;
-if ($test) {
-   map { $session->info("An old alignment is at ".$_->scaffold." at ".$_->s_insert) } $sSet->as_list;
-} else {
-   map { $_->delete } $sSet->as_list;
+
+my $bRS = new Blast_ReportSet($session,{-seq_name=>$seq->seq_name,
+                                          -db=>'release3_genomic'})->select;
+
+# lets be careful and make sure that we are only dealing with the
+# most recent blast run.
+
+my %runIds = ();
+my %hspIds = ();
+map { $runIds{$_->run_id} = 1 } $bRS->as_list;
+map { $hspIds{$_->id} = $_->run_id } $bRS->as_list;
+
+# how many do we have?
+$session->info("This sequence has multiple blast records.")
+                                             if scalar(keys %runIds) > 1;
+
+$session->die("There are no blast runs for this sequence.")
+                                           unless (scalar(keys %runIds));
+
+my $alignId = ( sort { $a <=> $b } keys %runIds )[-1];
+$session->info("Aligning blast run id $alignId.");
+
+# we want to see if we've already analyzed this blast set. We'll stop
+# if we see that we have alignments based on this set.
+my $saSet = new Seq_AlignmentSet($session,{-seq_name=>$seq->seq_name})->select;
+
+# have we already done this? Only check if we're not deleting them anyway
+unless ($deleteOld) {
+   map { $session->die("Latest blast run has already been aligned") if
+                        $hspIds{$_->hsp_id} == $alignId } $saSet->as_list;
 }
 
+# wrap everything in a transaction. We don't expect anyone to be
+# modifying these tables, but let's be careful.
+$session->db_begin;
 
-# todo: change this into a looping over BlastHsp objects
-#
-$sql = '';
+# now remove old if requested.
+$saSet->delete if $deleteOld;
 
-while (@records) {
-   my ($id,$name,$score,$percent,$match,$length,$query_begin,$query_end,
-             $subject_begin,$subject_end,$strand,$q_align,$s_align) = splice(@records,0,13);
+# a container for the new seq alignments.
+my $nsaSet = new Seq_AlignmentSet($session);
 
+foreach my $bH ($bRS->as_list) {
+   my $id = $bH->id;
+   my $name = $bH->name;
+   my $percent = $bH->percent;
+   my $match = $bH->match;
+   my $length = $bH->length;
+   my $query_begin = $bH->query_begin;
+   my $query_end = $bH->query_end;
+   my $subject_begin = $bH->subject_begin;
+   my $subject_end = $bH->subject_end;
+   my $q_align = $bH->query_align;
+   my $s_align = $bH->subject_align;
+   
    next if $percent < $percent_threshold;
    next if $length < $length_threshold;
    
    if ( ($length > $seq_length-10 && $length < $seq_length+10 ) &&
-        # todo: change this into an alignment object.
-        ($seq_length > 100  || $length/$seq_length >= .85) ) {
+        ($seq_length > 100  || $length/$seq_length >= .85)       ) {
 
       # here is the mapping of the insertion position onto the genomic
       # we want to find the exact location of the insert within the hsp
@@ -102,7 +141,7 @@ while (@records) {
       # the next case in somewhere in the middle. We'll count matches to
       # find the bestest mapping
       } elsif( ($insert_pos > $query_begin && $insert_pos < $query_end) ||
-          ($insert_pos < $query_begin && $insert_pos > $query_end) ) {
+               ($insert_pos < $query_begin && $insert_pos > $query_end) ) {
          $s_insert = $subject_begin;
          my $q_pos = $query_begin;
          my $q_inc = ($query_end>$query_begin)?+1:-1;
@@ -135,30 +174,38 @@ while (@records) {
          $s_insert = $subject_begin - ($insert_pos - $query_begin)
                                                       if ($extrapolate)
       } else {
-         die "internal inconsistency with insert position.";
+         $session->die("internal inconsistency with insert position.");
       }
 
-      $sql .= "insert into seq_alignment (seq_name,scaffold,p_start,p_end,s_start,";
-      $sql .= "s_end,s_insert,status,hsp_id) values (";
-      foreach my $var ($seq_name,$name,$query_begin,$query_end,$subject_begin,
-                                                         $subject_end,$s_insert,'multiple',$id) {
-         $sql .= $session->db->quote($var).",";
-      }
-      $sql =~ s/,$/);/;
-      $session->info("A new alignment is at ".$name." at ".$s_insert) if $test;
+      my $seqA = new Seq_Alignment($session,
+                                   {-seq_name => $seq_name,
+                                    -scaffold => $name,
+                                    -p_start  => $query_begin,
+                                    -p_end    => $query_end,
+                                    -s_start  => $subject_begin,
+                                    -s_end    => $subject_end,
+                                    -s_insert => $s_insert,
+                                    -status   => 'multiple',
+                                    -hsp_id   => $id});
+      $nsaSet->add($seqA);
    }
 }
 
   
-$session->log($Session::Verbose,"SQL: $sql.");
-$session->db->do($sql) if $sql && !$test;
-
-my $numHits = $session->db->select_value("select count(seq_name) from seq_alignment where ".
-                                         "seq_name='$seq_name'");
+my $numHits = $nsaSet->count;
 if( $numHits == 1 ) {
-   $session->log($Session::Verbose,"Declaring this alignment unique.");
-   $session->db->do(qq(update seq_alignment set status='unique' where
-                       seq_name='$seq_name' and status='multiple')) unless $test;
+   $session->verbose("Declaring this alignment unique.");
+   # map over the one element in the alignment
+   map {$_->status('unique') } $nsaSet->as_list;
+}
+
+# now do the insert
+$nsaSet->insert;
+
+if ($test) {
+   $session->db_rollback;
+} else {
+   $session->db_commit;
 }
 
 $session->exit();
