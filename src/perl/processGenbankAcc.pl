af3e97e328d8e8cc72b070bfcfa13d4516793ecd
joe <joe>
2005-02-28 18:52:59 +0000

    initial checkin. process acc number mail

---
+++ src/perl/processGenbankAcc.pl
@@ -0,0 +1,88 @@
+#!/usr/local/bin/perl -w -I../modules
+
+=head1 NAME
+
+   processGenBankAcc.pl
+
+   Process the file to insert GenBank accession id's into the db.
+
+=cut
+
+use Pelement;
+use PCommon;
+use Files;
+use Session;
+use Seq;
+use Submitted_Seq;
+
+use File::Basename;
+use Getopt::Long;
+
+# defaults
+
+my $verbose = 0;
+my $file;
+GetOptions("file=s"   => \$file,
+           "verbose!" => \$verbose);
+
+my $session = new Session;
+
+$session->log_level($Session::Verbose) if $verbose;
+
+$session->die("Need to supply a -file argument.") unless $file;
+open(FIL,$file) or $session->die("Cannot open $file: $!");
+
+my $ctr = 0;
+
+LINE:
+while(<FIL>) {
+   chomp $_;
+
+   # skip headers and blank lines
+   next unless $_;
+   next if /^dbGSS_Id/;
+   next if /^==/;
+
+   my ($dbgss,$name,$acc) = split(/\s+/,$_);
+
+   $session->warn("Cannot parse the line: $_") unless $dbgss && $name && $acc;
+
+   $name =~ s/prime$//;
+
+   unless ( new Seq($session,{-name=>$name})->db_exists ) {
+      $session->die("There is no sequence named $name.");
+   }
+
+   my $sseq = new Submitted_Seq($session,{-seq_name=>$name});
+
+
+   # eventually, we'll make this more sophisitcated. For now, we're only
+   # going to put in new records only.
+   $session->die("Already have a db entry for $name.") if $sseq->db_exists;
+
+   # this is irrelevant for now
+   if ($sseq->dbgss_id  || $sseq->gb_acc) {
+      if ( $sseq->dbgss_id ne $dbgss) {
+         $session->die("There is a genbank id for $name and it disagrees.");
+      } elsif ( $sseq->gb_acc ne $acc) {
+         $session->die("There is a genbank accession for $name and it disagrees.");
+      } elsif ( $sseq->gb_acc eq $acc && $sseq->dbgss_id eq $dbgss) {
+         $session->warn("There is already an accession and gss id for $name.");
+         next LINE;
+      }
+   }
+
+   $sseq->dbgss_id($dbgss);
+   $sseq->gb_acc($acc);
+   $sseq->submission_date('today');
+   $sseq->insert;
+   $ctr++;
+}
+     
+close(FIL);
+
+$session->info("Updated $ctr records.");
+
+$session->exit;
+exit(0);
+
