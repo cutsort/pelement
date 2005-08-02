b5787f23a87bb3bc527c378fe4c68ab2e7ba3e89
joe <joe>
2005-08-02 21:39:06 +0000

    test mode

--- src/perl/generateGenBankMail.pl
+++ src/perl/generateGenBankMail.pl
@@ -46,15 +46,20 @@ my $session = new Session();
 my $outFile;
 my $appendFile;
 my $minLength = 25;
+
 # only submit aligned strains?
 my $ifAligned = 1;
 # if no joint sequence, individual flanks?
 my $subseq = 1;
+# do not commit changes
+my $test = 0;
+
 GetOptions('out=s'      => \$outFile,
            'append=s'   => \$appendFile,
            'min=i'      => \$minLength,
            'ifaligned!' => \$ifAligned,
-           'subseq!'    => \$subseq
+           'subseq!'    => \$subseq,
+           'test!'      => \$test
           );
 
 if ($appendFile && -e $appendFile) {
@@ -75,16 +80,17 @@ if ($appendFile && -e $appendFile) {
 }
 
 
+$session->db_begin if $test;
+
 ARG:
 foreach my $arg (@ARGV) {
 
-   # both end seqs
-   my %ends = ();
-   # both insertion positions
-   my %pos = ();
+   # all the labeled insertions
+   my %insertions = ();
 
    $session->log($Session::Info,"Processing $arg.");
 
+   # this is the strain identifier; the insertion id may differ from this
    my ($strain,$end,$qual) = Seq::parse($arg);
 
    # make sure this is a known sequence;
@@ -102,7 +108,7 @@ foreach my $arg (@ARGV) {
    }
 
    # we handle the cases of a specified end differently from only
-   # a strain designator
+   # a strain designator. An argument with an end only retrieves that sequence
 
    if ($end && $end ne 'b') {
       my $seq = new Seq($session,{-seq_name=>$arg});
@@ -118,8 +124,10 @@ foreach my $arg (@ARGV) {
          ($session->warn("No alignments for $arg. Skipping.") and next ARG) unless 
                           $sA_curated->db_exists || $sA_unique->db_exists;
       }
-      $ends{$end} = $seq->sequence;
-      $pos{$end} = $seq->insertion_pos;
+
+      $insertions{$strain} = {ends=>{},pos=>{}} unless exists $insertions{$strain};
+      $insertions{$strain}->{ends}{$end} = $seq->sequence;
+      $insertions{$strain}->{pos}{$end} = $seq->insertion_pos;
       
    } elsif ($subseq) {
       # find the associated sequences
@@ -136,13 +144,18 @@ foreach my $arg (@ARGV) {
       SEQ:
       foreach my $seq ($seqSet->as_list) {
          my ($this_strain,$this_end,$this_qual) = $seq->parse;
+
          # only submit unqualified sequences.
          next if $this_qual;
-         if ($ends{$this_end}) {
+
+         # a hack to bypass the Exelixis cutouts
+         next if $this_strain =~ /cut/;
+
+         if ($insertions{$this_strain}->{ends}{$this_end}) {
             $session->warn("This cannot deal with multiple insertions yet. Skipping.");
             next SEQ;
          }
-         unless ($seq->sequence && $seq->insertion_pos) {
+         unless ($seq->sequence && ($seq->insertion_pos ne '') ) {
             $session->warn("Sequence record for ".$seq->seq_name." is missing information. Skipping.");
             next SEQ;
          }
@@ -153,58 +166,72 @@ foreach my $arg (@ARGV) {
            ($session->warn("No alignments for ".$seq->seq_name.". Skipping.") and next SEQ) unless 
                             $sA_curated->db_exists || $sA_unique->db_exists;
          }
-         $ends{$this_end} = $seq->sequence;
-         $pos{$this_end} = $seq->insertion_pos;
+         $insertions{$this_strain} = {ends=>{},pos=>{}} unless exists $insertions{$this_strain};
+         $insertions{$this_strain}->{ends}{$this_end} = $seq->sequence;
+         $insertions{$this_strain}->{pos}{$this_end} = $seq->insertion_pos;
       }
    }
 
-   if (exists($ends{b}) && length($ends{b}) >= $minLength ) {
-      # if we have a 'both' end, we're submitting that.
 
-      # if we're requiring alignment, then we need to see that at least 1 flanking seq has
-      # an alignment
-      if ($ifAligned) {
-         ($session->warn("No alignments for either end of $arg. Skipping") and next) unless
-                 exists($ends{5}) || exists($ends{3});
-      }
-      my $gb = new GenBank_Submission_Info($session,{-collection=>$st->collection})->select;
-      $gb->gss($st->strain_name);
-      # was this submitted before?
-      $gb->status('Update') if (new Submitted_Seq($session,{-seq_name=>$st->strain_name.'-'.$end})->db_exists);
+   # we've prepared a hash of %insertions; this has the info
+   # needed for our submission
 
-      $gb->dbxref($st->strain_name);
-      $gb->add_seq('b',$ends{b},$pos{b});
-      my $p_end = $gb->p_end;
-      $p_end =~ s/<ENDDESCR>/Both 5' and 3' ends/;
-      $gb->p_end($p_end);
-      print FIL $gb->print ,"\n";
-   } else {
-      # otherwise, both ends are separate submissions
-      foreach my $end qw(3 5) {
-         if ( exists($ends{$end}) && length($ends{$end}) >= $minLength ) {
-            my $gb = new GenBank_Submission_Info($session,{-collection=>$st->collection})->select;
-            $gb->gss($st->strain_name.'-'.$end.'prime');
+   foreach my $insert (keys %insertions) {
 
-            # was this submitted before?
-            $gb->status('Update') if (new Submitted_Seq($session,{-seq_name=>$st->strain_name.'-'.$end})->db_exists);
+      # simplify the hash derefencing
+      my %ends = %{$insertions{$insert}->{ends}};
+      next unless exists $insertions{$insert}->{pos};
+      my %pos = %{$insertions{$insert}->{pos}};
 
-            $gb->dbxref($st->strain_name);
-            $gb->add_seq($end,$ends{$end},$pos{$end});
-            my $end_txt = $end."' end";
-            my $p_end = $gb->p_end;
-            $p_end =~ s/<ENDDESCR>/$end_txt/;
-            $gb->p_end($p_end);
-            print FIL $gb->print ,"\n";
+      if (exists($ends{b}) && length($ends{b}) >= $minLength ) {
+         # if we have a 'both' end, we're submitting that.
+
+         # if we're requiring alignment, then we need to see that at
+         # least 1 flanking seq has an alignment
+         if ($ifAligned) {
+            ($session->warn("No alignments for either end of $arg. Skipping") and next) unless
+                    exists($ends{5}) || exists($ends{3});
+         }
+         my $gb = new GenBank_Submission_Info($session,{-collection=>$st->collection})->select;
+         $gb->gss($insert);
+         # was this submitted before?
+         $gb->status('Update') if (new Submitted_Seq($session,{-seq_name=>$insert.'-'.$end})->db_exists);
+
+         $gb->dbxref($insert);
+         $gb->add_seq('b',$ends{b},$pos{b});
+         my $p_end = $gb->p_end;
+         $p_end =~ s/<ENDDESCR>/Both 5' and 3' ends/;
+         $gb->p_end($p_end);
+         print FIL $gb->print ,"\n";
+      } else {
+         # otherwise, both ends are separate submissions
+         foreach my $end qw(3 5) {
+            if ( exists($ends{$end}) && length($ends{$end}) >= $minLength ) {
+               my $gb = new GenBank_Submission_Info($session,{-collection=>$st->collection})->select;
+               $gb->gss($insert.'-'.$end.'prime');
+
+               # was this submitted before?
+               $gb->status('Update') if (new Submitted_Seq($session,{-seq_name=>$insert.'-'.$end})->db_exists);
+
+               $gb->dbxref($insert);
+               $gb->add_seq($end,$ends{$end},$pos{$end});
+               my $end_txt = $end."' end";
+               my $p_end = $gb->p_end;
+               $p_end =~ s/<ENDDESCR>/$end_txt/;
+               $gb->p_end($p_end);
+               print FIL $gb->print ,"\n";
+            }
          }
       }
-   }
        
+   }
    $session->log($Session::Info,"Submission info for $arg generated.");
 
 }
 
 close(FIL);
 
+$session->db_rollback if $test;
 $session->exit();
 
 exit(0);
