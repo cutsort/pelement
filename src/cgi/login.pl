9fe71e187dc978c7dfcfe64e252e3592d947f425
joe <joe>
2005-12-12 18:21:30 +0000

    first real version that works

--- src/cgi/login.pl
+++ src/cgi/login.pl
@@ -1,55 +1,91 @@
 #!/usr/local/bin/perl -I../modules
 
+=head1 NAME
+
+  login.pl Web user_id setter
+
+=cut
+
 use Pelement;
 use PelementCGI;
-use WebSession;
+use Session;
+
+use CGI::FormBuilder;
+use CGI::Session qw/-ip-match/;
+
+use strict;
 
 my $cgi = new PelementCGI;
 
-my $user = $cgi->param('user');
-my $pass = $cgi->param('pass');
-my $referer = $cgi->param('referer') || $ENV{HTTP_REFERER};
+my $s = new Session({-log_level=>0});
+CGI::Session->name("PELEMENTSID");
+my $cgiSession = new CGI::Session("driver:PostgreSQL", $cgi, {Handle=>$s->db});
 
-print $cgi->header(),$cgi->banner(),"\n";
+my $form = new CGI::FormBuilder(
+           fields => [qw(Name)],
+           header => 0,
+           method => 'GET',
+           );
 
-if (!$user && !$pass) {
-   print
-      $cgi->start_html(-title => "Pelement Login", -bgcolor => "#fcffff"),
-      $cgi->center(
-         $cgi->h3("Login to the Pelement Project Web Site"),"\n",
-            $cgi->br,
-               $cgi->start_form(-method=>"post",-action=>"/cgi-bin/pelement/login.cgi"),"\n",
-                  $cgi->table(
-                     $cgi->Tr( [
-                        $cgi->td({-align=>"right",-align=>"left"},
-                                          ["User:",$cgi->textfield(-name=>"user")]),
-                        $cgi->td({-align=>"right",-align=>"left"},
-                                          ["Password:",$cgi->password_field(-name=>"pass")]),
-                        $cgi->td({-colspan=>2,-align=>"center"},[$cgi->submit(-name=>"Login")])
-                               ]
-                     ),"\n",
-                  ),
-               $cgi->end_form(),"\n",
-         $cgi->em(qq(This site uses cookies to track logins in the database. If cookies are
-                     disabled in your browser then you will be able to query the database without
-                     typing in your password as often.)),
-      ),"\n";
+if (my $id = $cgiSession->param('user_id')) {
+  print $cgi->p($cgi->em("You are currently logged in as ",$cgi->b($id)),
+        $cgi->a({-href=>'logout.pl'},'Logout')),
+        $cgi->hr,"\n";
+}
+
+if ($form->submitted && $form->validate && (my $id = $form->fields->{Name}) ) {
+
+  # see if this is a real person
+  # we should have checked at $id is not null at
+  # the validate stage, but check again
+  if ($id && $s->Person({-login=>$id})->db_exists ) {
+    # set the user id field for this session
+    $cgiSession->param('user_id',$id);
+
+    my $back_url = $cgiSession->param('referrer');
+    if ($back_url) {
+      print $cgi->header();
+      print $cgi->init_page({-title=>'Login Successful',
+          -head=>
+           ["<META HTTP-EQUIV=\"refresh\" content=\"2; URL=$back_url\"/>"]});
+      print $cgi->banner();
+      print $cgi->p("Hello, $id\n",$cgi->br,
+                   'Your browser should return to the previous form, or ',
+                    $cgi->a({-href=>$back_url},'click'),' to continue.');
+    } else {
+      print $cgi->header();
+      print $cgi->init_page({-title=>"Login Successful"});
+      print $cgi->banner();
+      print $cgi->p("Hello, $id\n",$cgi->br,
+        "Your login was successful, but there is no referring URL for return.");
+    }
+  } else {
+    print $cgi->header;
+    print $cgi->init_page({-title=>"Login Unsuccessful"});
+    print $cgi->banner;
+    print $cgi->p("Your login name could not be found in the db.");
+    print $cgi->center($form->render);
+  }
 
 } else {
 
-   my $db = PelementDBI($PELEMENT_DB_CONNECT);
-
-   my $storedPass = $db->select(qq(select code from person where login='$user'));
-
-   if (crypt($pass,salt(substr($storedPass,0,2))) eq $storedPass ) {
-      # create a session identifier based on this username.
-      my $webSession = new WebSession($db,$user);
-      print $cgi->p(qq(Login was successful. Session identifier is ).$webSession->webId),"\n";
-   }
-
-   $db->disconnect();
-  
+  print $cgi->header();
+  print $cgi->init_page({-title=>"Login"});
+  print $cgi->banner();
+  print $cgi->center(
+        $cgi->h3("Authentification Required."),
+        $cgi->em("Please enter your login name for access to these pages."));
+  print $cgi->center($form->render);
 }
 
-print $cgi->footer,$cgi->end_html,"\n";
+print $cgi->footer([
+                 {link=>"batchReport.pl",name=>"Batch Report"},
+                 {link=>"strainReport.pl",name=>"Strain Report"},
+                 {link=>"gelReport.pl",name=>"Gel Report"},
+                 {link=>"strainStatusReport.pl",name=>"Strain Status Report"},
+                  ]);
 
+print $cgi->close_page();
+
+$s->exit;
+exit(0);
