43ed0f1b0fc2d52b3eadcde73f1d3c5ee8a44380
joe <joe>
2007-10-16 16:14:53 +0000

    initial checkin: cgi for downloading fasta

---
+++ src/cgi/seqDownload.pl
@@ -0,0 +1,56 @@
+#!/usr/local/bin/perl -I../modules
+
+=head1 NAME
+
+  seqDownload.pl Web script for downloading sequence
+
+=cut
+
+use Pelement;
+use Session;
+use PelementCGI;
+
+$cgi = new PelementCGI;
+
+
+if ($cgi->param('seq_name')) {
+   spewSeq($cgi);
+} else {
+   print $cgi->header();
+   print $cgi->init_page({-title=>"Sequence Report"});
+   print $cgi->banner();
+   selectSeq($cgi);
+   print $cgi->footer();
+   print $cgi->close_page();
+}
+
+exit(0);
+
+sub selectSeq
+{
+
+   my $cgi = shift;
+
+   # still need to write
+
+}
+
+sub spewSeq
+{
+   my $cgi = shift;
+   my $session = new Session({-log_level=>0});
+
+   my $seq = $session->Seq({-seq_name=>$cgi->param('seq_name')})->select_if_exists;
+   if ($seq->sequence) {
+     print $cgi->header(-type=>"text/plain",
+            -content_disposition=>"attachment; filename=".$seq->seq_name.'.fasta');
+
+     my $bases = $seq->sequence;
+     $bases =~ s/(.{50})/$1\n/g;
+     $bases .= "\n";
+     $bases =~ s/\n\n/\n/g;
+     print ">",$seq->seq_name,"\n",$bases;
+
+   }
+   $session->exit;
+}
