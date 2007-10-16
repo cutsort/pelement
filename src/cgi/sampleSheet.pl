5d504fad6cf7ef9db5d9e7fc0c8de1b682b71fed
joe <joe>
2007-10-16 16:11:17 +0000

    header/footer

--- src/cgi/sampleSheet.pl
+++ src/cgi/sampleSheet.pl
@@ -19,10 +19,8 @@ use strict;
 my $cgi = new PelementCGI;
 my $s = new Session({-log_level=>0});
 
-print $cgi->header;
-print $cgi->init_page({-title=>"Sequencing Sample Sheets"});
-print $cgi->banner();
 
+# defer writing the header until we see what we got.
 my $form = new CGI::FormBuilder(
            header => 0,
            method => 'POST',
@@ -33,19 +31,28 @@ my $form = new CGI::FormBuilder(
 
 $form->field(name=>'gel',label=>'Gel Name');
 
+
+
 if ($form->submitted && $form->validate) {
 
     my $gel = $form->field('gel');
     my $g = $s->Gel({-name => $gel})->select_if_exists;
 
     unless ($g->id) {
-      print $cgi->p($cgi->em("There is no such gel."));
+      ##print $cgi->p($cgi->em("There is no such gel."));
     } else {
-      print $cgi->pre($g->sample_sheet);
+      print $cgi->header(-type=>'text/plain',);
+                   ##-content_disposition=>'attachment; filename=sample.plt');
+      print $g->sample_sheet;
+      $s->exit;
+      exit(0);
     }
 
 } else {
 
+    print $cgi->header;
+    print $cgi->init_page({-title=>"Sequencing Sample Sheets"});
+    print $cgi->banner();
     print $cgi->center($cgi->hr,
                        $form->render(submit=>['Enter']));
 
@@ -59,7 +66,6 @@ print $cgi->footer([
                   ]);
 
 print $cgi->close_page();
-
 
 $s->exit;
 exit(0);
