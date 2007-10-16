ba1f50ddeffd9108a039b6d12bc92fc446c448ba
joe <joe>
2007-10-16 16:15:12 +0000

    initial chekin: cgi for recent batch list

---
+++ src/cgi/recentBatches.pl
@@ -0,0 +1,64 @@
+#!/usr/local/bin/perl -I../modules
+
+
+=head1 Name
+
+recentBatches.pl The summary page recent batches
+
+=head1 Description
+
+Print a page with batches done in the last 2 weeks (configurable?)
+with links
+  
+
+=cut
+
+use Pelement;
+use PelementCGI;
+use Session;
+use BatchSet;
+
+use CGI::FormBuilder;
+
+use strict;
+
+my $cgi = new PelementCGI;
+
+my $s = new Session({-log_level=>0});
+
+print $cgi->header;
+print $cgi->init_page({-title=>"P Element Batch Summary",
+                       -style=>{-src=>'/pelement/pelement.css'}});
+print $cgi->banner;
+
+my $days = $cgi->param('day') || 30;
+my $one_month_ago = PCommon::time_value(time()-$days*24*60*60);
+my $batch = new BatchSet($s,{-greater_than_or_equal=>
+              {batch_date=>$one_month_ago}})->select;
+
+if ( $batch->count ) {
+
+my @contents = ();
+map { push @contents, [ $cgi->a({-href=>'batchReport.pl?batch='.$_->id},'Batch '.$_->id), $_->batch_date ] }
+           sort { $b->batch_date cmp $a->batch_date } $batch->as_list;
+
+print $cgi->center( "There are ".$batch->count." recent batches.",
+                    $cgi->br,
+                    $cgi->table({-width=>'70%',
+                                 -class=>'unboxed'},
+                            $cgi->Tr( [
+                            $cgi->th({-background-color=>'gray',
+                                      -class=>'unboxed'},['Batch','Registration Date']),
+                              ( map { $cgi->td({-class=>'unboxed'},$_)} @contents )]
+                              ))),"\n";
+
+
+} else {
+  print $cgi->center("There are no recent batches.");
+}
+print $cgi->footer();
+print $cgi->close_page();
+
+$s->exit;
+
+exit(0);
