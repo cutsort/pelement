b1134331d6e22f26d42f6208f2ef8b17143f6647
joe <joe>
2009-02-27 00:27:08 +0000

    initial

---
+++ src/modules/BatchSet.pm
@@ -0,0 +1,19 @@
+package BatchSet;
+
+=head1 Name
+
+   BatchSet.pm   A module for the db interface for sets of batch thingies.
+
+=head1 Usage
+
+   use BatchSet;
+   $batchSet = new BatchSet([options]);
+
+=cut
+
+use strict;
+use Pelement;
+use PCommon;
+use DbObjectSet;
+
+1;
