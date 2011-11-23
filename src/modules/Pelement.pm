77d1f96556893cff9dd5843885a758c1e8e13ad1
joe <joe>
2011-11-23 22:33:21 +0000

    update db

--- src/modules/Pelement.pm
+++ src/modules/Pelement.pm
@@ -61,7 +61,7 @@ $PELEMENT_NOTIFY = "joe\@fruitfly.org";
 # db server
 $PELEMENT_DB_DBI     = "Pg";
 # whatever it takes to connect.
-$PELEMENT_DB_CONNECT = "dbname=pelement;host=staufen.lbl.gov";
+$PELEMENT_DB_CONNECT = "dbname=pelement;host=eel.lbl.gov";
 
 # other executable directories
 $GENOMIC_BIN = "/usr/local/bdgp/bin/";
