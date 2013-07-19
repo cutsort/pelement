c61477e699525d9f422e3d4c77892c2481b84e24
Ben Booth <benwbooth@gmail.com>
2013-07-19 14:10:10 -0700

    use text::csv to parse the tsv file

--- src/perl/importPhenotype.pl
+++ src/perl/importPhenotype.pl
@@ -20,6 +20,7 @@ use Stock_Record;
 
 use File::Basename;
 use Getopt::Long;
+use Text::CSV;
 
 # defaults
 
@@ -45,37 +46,37 @@ my $ctr = 0;
 # field index to column x reference
 my %xref;
 
+my $csv = Text::CSV->new({binary=>1, sep_char=>"\t"});
+
 LINE:
-while(<FIL>) {
-   chomp $_;
+while(my $fields = $csv->getline(\*FIL)) {
 
    # skip blank lines
-   next unless $_;
+   next unless @$fields;
 
-   if ( /Strain/ ) {
+   if ( grep {/Strain/} @$fields ) {
       # a header line. Let's scan it to be sure we're still happy
-      my @fields = split(/\t/,$_);
-      foreach my $i (0..$#fields) {
-         if ( $fields[$i] =~ /^strain/i ) {
+      foreach my $i (0..$#$fields) {
+         if ( $fields->[$i] =~ /^strain/i ) {
            $xref{strain} = $i;
-         } elsif ( $fields[$i] =~ /^is[_ ]multi.?$/i ) {
+         } elsif ( $fields->[$i] =~ /^is[_ ]multi.?$/i ) {
            $xref{is_multi} = $i;
-         } elsif ( $fields[$i] =~ /^is[_ ]multi[_ ]comment$/i ) {
+         } elsif ( $fields->[$i] =~ /^is[_ ]multi[_ ]comment$/i ) {
            $xref{is_multi_comment} = $i;
-         } elsif ( $fields[$i] =~ /^is[_ ]homo[_ ]viable/i ) {
+         } elsif ( $fields->[$i] =~ /^is[_ ]homo[_ ]viable/i ) {
            $xref{is_homozygous_viable} = $i;
-         } elsif ( $fields[$i] =~ /^is[_ ]homo[_ ]fertile/i ) {
+         } elsif ( $fields->[$i] =~ /^is[_ ]homo[_ ]fertile/i ) {
            $xref{is_homozygous_fertile} = $i;
-         } elsif ( $fields[$i] =~ /^phenotype$/i ) {
+         } elsif ( $fields->[$i] =~ /^phenotype$/i ) {
            $xref{phenotype} = $i;
-         } elsif ( $fields[$i] =~ /^gen.*pos.*comm.*/i ) {
+         } elsif ( $fields->[$i] =~ /^gen.*pos.*comm.*/i ) {
            $xref{genome_position_comment} = $i;
-         } elsif ( $fields[$i] =~ /^insertion$/i ) {
+         } elsif ( $fields->[$i] =~ /^insertion$/i ) {
            $xref{insertion} = $i;
-         } elsif ( $fields[$i] =~ /^fbid$/i ) {
+         } elsif ( $fields->[$i] =~ /^fbid$/i ) {
            $xref{fbid} = $i;
          } else {
-           $session->warn("Unprocessed field: $fields[$i].");
+           $session->warn("Unprocessed field: $fields->[$i].");
          }
       }
       if ($header) {
@@ -93,16 +94,14 @@ while(<FIL>) {
       $session->die("This may need rework.") if ( defined($xref{$_}) && ($xref{$_} == 0) &&  ($_ ne 'strain') )
        } keys %xref;
 
-   my @fields = split(/\t/,$_);
-
-   unless ( new Strain($session,{-strain_name=>$fields[$xref{strain}]})->db_exists ) {
-      $session->die("There is no strain named $fields[$xref{strain}].");
+   unless ( new Strain($session,{-strain_name=>$fields->[$xref{strain}]})->db_exists ) {
+      $session->die("There is no strain named $fields->[$xref{strain}].");
    }
 
    my $action;
-   my $pheno = new Phenotype($session,{-strain_name=>$fields[$xref{strain}]});
+   my $pheno = new Phenotype($session,{-strain_name=>$fields->[$xref{strain}]});
    if ($pheno->db_exists) {
-      $session->info("Using existing record for $fields[$xref{strain}].");
+      $session->info("Using existing record for $fields->[$xref{strain}].");
       $pheno->select;
       $action = 'update';
    } else {
@@ -113,36 +112,36 @@ while(<FIL>) {
    # various checks. the is_* fields can only be 'Y', 'N', 'U',  or 'P' (or blank)
    foreach my $f ($xref{is_multi},$xref{is_homozygous_viable},$xref{is_homozygous_fertile}) {
       next unless $f;
-      $session->die("Field $f is not valid: $fields[$f]") unless
-          (!$fields[$f] || $fields[$f] eq 'Y' || $fields[$f] eq 'N'
-                        || $fields[$f] eq 'U' || $fields[$f] eq 'P');
+      $session->die("Field $f is not valid: $fields->[$f]") unless
+          (!$fields->[$f] || $fields->[$f] eq 'Y' || $fields->[$f] eq 'N'
+                        || $fields->[$f] eq 'U' || $fields->[$f] eq 'P');
    }
-   $pheno->is_multiple_insertion($fields[$xref{is_multi}])
-                      if $xref{is_multi} && $fields[$xref{is_multi}];
-   $pheno->strain_comment($fields[$xref{is_multi_comment}])
-                      if $xref{is_multi_comment} && $fields[$xref{is_multi_comment}];
-   $pheno->is_homozygous_viable($fields[$xref{is_homozygous_viable}])
-                      if $xref{is_homozygous_viable} && $fields[$xref{is_homozygous_viable}];
-   $pheno->is_homozygous_fertile($fields[$xref{is_homozygous_fertile}])
-                      if $xref{is_homozygous_fertile} && $fields[$xref{is_homozygous_fertile}];
-   $pheno->phenotype($fields[$xref{phenotype}])
-                      if $xref{phenotype} && $fields[$xref{phenotype}];
-   $pheno->phenotype_comment($fields[$xref{genome_position_comment}])
-                      if $xref{genome_position_comment} && $fields[$xref{genome_position_comment}];
+   $pheno->is_multiple_insertion($fields->[$xref{is_multi}])
+                      if $xref{is_multi} && $fields->[$xref{is_multi}];
+   $pheno->strain_comment($fields->[$xref{is_multi_comment}])
+                      if $xref{is_multi_comment} && $fields->[$xref{is_multi_comment}];
+   $pheno->is_homozygous_viable($fields->[$xref{is_homozygous_viable}])
+                      if $xref{is_homozygous_viable} && $fields->[$xref{is_homozygous_viable}];
+   $pheno->is_homozygous_fertile($fields->[$xref{is_homozygous_fertile}])
+                      if $xref{is_homozygous_fertile} && $fields->[$xref{is_homozygous_fertile}];
+   $pheno->phenotype($fields->[$xref{phenotype}])
+                      if $xref{phenotype} && $fields->[$xref{phenotype}];
+   $pheno->phenotype_comment($fields->[$xref{genome_position_comment}])
+                      if $xref{genome_position_comment} && $fields->[$xref{genome_position_comment}];
 
    $pheno->$action;
 
    if (exists($xref{fbid}) ) {
-      my $stock = new Stock_Record($session,{-strain_name=>$fields[$xref{strain}],
-                                             -fbti=>$fields[$xref{fbid}]});
+      my $stock = new Stock_Record($session,{-strain_name=>$fields->[$xref{strain}],
+                                             -fbti=>$fields->[$xref{fbid}]});
       unless ( $stock->db_exists ) {
-        $stock->insertion(($xref{insertion}&&$fields[$xref{insertion}])?
-                          $fields[$xref{insertion}]:$fields[$xref{strain}]);
+        $stock->insertion(($xref{insertion}&&$fields->[$xref{insertion}])?
+                          $fields->[$xref{insertion}]:$fields->[$xref{strain}]);
         $stock->insert;
       } else {
         $stock->select;
-        $stock->insertion(($xref{insertion}&&$fields[$xref{insertion}])?
-                          $fields[$xref{insertion}]:$fields[$xref{strain}]);
+        $stock->insertion(($xref{insertion}&&$fields->[$xref{insertion}])?
+                          $fields->[$xref{insertion}]:$fields->[$xref{strain}]);
         $stock->update;
       }
    }
